34ccf400e8724706fd56b6add55bba4d239e9c85
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 15:44:07 +0100

    add: Part05-14 - Books

---
+++ course-1/part-05/part05-Part05_14.Books/src/main/java/Book.java
@@ -0,0 +1,35 @@
+
+public class Book {
+
+    private String name;
+    private int publicationYear;
+
+    public Book(String name, int publicationYear) {
+        this.name = name;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    @Override
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+
+        if (!(compared instanceof Book)) {
+            return false;
+        }
+
+        Book newBook = (Book) compared;
+
+        return this.name.equals(newBook.getName()) && this.publicationYear == newBook.getPublicationYear();
+    }
+
+}
