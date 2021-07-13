ea18ce6361d52b39860ba9a22a1a3176aa1d02ce
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 15:56:18 +0100

    add: Part06-09 - SimpleDictionary

---
+++ course-1/part-06/part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
@@ -0,0 +1,57 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author lucas
+ */
+public class TextUI {
+
+    private Scanner scanner;
+    private SimpleDictionary dictionary;
+
+    public TextUI(Scanner scanner, SimpleDictionary dictionary) {
+        this.scanner = scanner;
+        this.dictionary = dictionary;
+    }
+
+    public void start() {
+        
+        while (true) {
+            System.out.print("Command: ");
+            String command = this.scanner.nextLine();
+
+            if (command.equals("end")) {
+                System.out.println("Bye bye!");
+                break;
+            }
+
+            if (command.equals("add")) {
+                System.out.print("Word: ");
+                String word = this.scanner.nextLine();
+                System.out.print("Translation: ");
+                String translation = this.scanner.nextLine();
+                this.dictionary.add(word, translation);
+            }
+            if (command.equals("search")) {
+                System.out.print("To be translated: ");
+                String word = this.scanner.next();
+
+                String translation = this.dictionary.translate(word);
+
+                if (translation == null) {
+                    System.out.println("Word " + word + " was not found");
+                } else {
+                    System.out.println("Translation: " + translation);
+                }
+            } else {
+                System.out.println("Unknown command");
+            }
+        }
+    }
+}
