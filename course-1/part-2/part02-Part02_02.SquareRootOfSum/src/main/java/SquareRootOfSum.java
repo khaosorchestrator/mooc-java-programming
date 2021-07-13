4cc4a252099b498ddf5021b9a571275a1c1c842e
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 14:48:58 +0100

    add: Part02-02 - SquareRootOfSum

---
+++ course-1/part-2/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class SquareRootOfSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int number1 = Integer.valueOf(scanner.next());
+        int number2 = Integer.valueOf(scanner.next());
+        double squareRoot = Math.sqrt(number1 + number2);
+        
+        System.out.println(squareRoot);
+    }
+}
