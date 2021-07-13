d0511730690aa678d8ec514cd3d3fdbc395d03c3
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 15:31:47 +0100

    add: Part04-19 - TelevisionPrograms

---
+++ course-1/part-04/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // implement here your program that uses the TelevisionProgram class
+
+        ArrayList<TelevisionProgram> programs = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+
+            if (name.isEmpty()) {
+                break;
+            }
+            
+            System.out.print("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+            
+            programs.add(new TelevisionProgram(name, duration));
+        }
+        
+        System.out.println("");
+        System.out.print("Program's maximum duration? ");
+        int duration = scanner.nextInt();
+        
+        for (TelevisionProgram program: programs) {
+            if (program.getDuration() <= duration) {
+                System.out.println(program);
+            }
+        }
+        
+
+    }
+}
