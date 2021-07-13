d93729614d380f9bcbf80226458dee91c920810c
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 14:49:16 +0100

    add: Part02-03 - AbsoluteValue

---
+++ course-1/part-2/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.valueOf(scanner.next());
+        
+        if (number < 0) {
+            System.out.println(number * (-1));
+        } else {
+            System.out.println(number);
+        }
+    }
+}
