b4550ea444d811d2c6cdb38c942aaf00eed8749a
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 14:49:40 +0100

    add: Part02-04 - ComparingNumbers

---
+++ course-1/part-2/part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class ComparingNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = Integer.valueOf(scanner.next());
+        int number2 = Integer.valueOf(scanner.next());
+
+        if (number1 > number2) {
+            System.out.println(number1 + " is greater than " + number2 + ".");
+        } else if (number1 < number2) {
+            System.out.println(number1 + " is smaller than " + number2 + ".");
+        } else {
+            System.out.println(number1 + " is equal to " + number2 + ".");
+        }
+    }
+}
