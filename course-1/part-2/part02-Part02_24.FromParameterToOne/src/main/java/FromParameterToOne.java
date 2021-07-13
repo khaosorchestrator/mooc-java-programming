25e8cfc2da705335c09fd45c41b16a350912fd00
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 14:57:58 +0100

    add: Part02-24 - FromParameterToOne

---
+++ course-1/part-2/part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -0,0 +1,14 @@
+
+public class FromParameterToOne {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void printFromNumberToOne(int number) {
+        while (number >= 1) {
+            System.out.println(number);
+            number--;
+        }
+    }
+}
