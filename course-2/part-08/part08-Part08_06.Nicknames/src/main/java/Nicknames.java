182a62f23c661a25ffb583bfdf1cef3b5df9f8ac
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 16:06:57 +0100

    add: Part08-06 - Nicknames

---
+++ course-2/part-08/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -0,0 +1,14 @@
+
+import java.util.HashMap;
+
+public class Nicknames {
+
+    public static void main(String[] args) {
+        HashMap<String, String> names = new HashMap<>();
+
+        names.put("matthew's", "matt");
+        names.put("michael's", "mix");
+        names.put("arthur's", "artie");
+    }
+
+}
