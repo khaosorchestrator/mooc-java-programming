e5cabcee17c909c74d7754e408b0f845da67e665
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 15:54:23 +0100

    add: Part06-06 - LongestInCollection

---
+++ course-1/part-06/part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+
+public class SimpleCollection {
+
+    private String name;
+    private ArrayList<String> elements;
+
+    public SimpleCollection(String name) {
+        this.name = name;
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(String element) {
+        this.elements.add(element);
+    }
+
+    public ArrayList<String> getElements() {
+        return this.elements;
+    }
+
+    public String longest() {
+        if (elements.isEmpty()) {
+            return null;
+        }
+
+        int longestEl = elements.get(0).length();
+        String longestStr = null;
+
+        for (String element : elements) {
+            if (element.length() > longestEl) {
+                longestEl = element.length();
+                longestStr = element;
+            }
+        }
+        
+        return longestStr;
+    }
+
+}
