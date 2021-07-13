4ccd77aa04b463f4c41f9a1704ecb692c79c7d14
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 16:13:37 +0100

    add: Part09-07 - ListAsAMethodParameter

---
+++ course-2/part-09/part09-Part09_07.ListAsAMethodParameter/src/main/java/mainProgram.java
@@ -0,0 +1,17 @@
+
+import java.util.List;
+
+public class mainProgram {
+
+    public static void main(String[] args) {
+        // test your method here
+
+    }
+
+    public static int returnSize(List data) {
+        return data.size();
+    }
+    // Implement here a method returnSize
+    // which returns the size of the list given to it
+    //as a parameter
+}
