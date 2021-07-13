3cacec0325fd1d238157394203b3bcf408f0f461
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 15:10:34 +0100

    add: Part03-12 - SumOfAList

---
+++ course-1/part-3/part03-Part03_12.SumOfAList/src/main/java/SumOfAList.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SumOfAList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int sum = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+            sum += input;
+        }
+
+        System.out.println("Sum: " + sum);
+
+        // toteuta listan lukujen summan laskeminen tänne
+    }
+}
