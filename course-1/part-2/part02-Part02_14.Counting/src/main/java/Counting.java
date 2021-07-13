a2d8614b917b73f5b7845a69a3d846e55a53651b
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 14:53:24 +0100

    add: Part02-14 - Counting

---
+++ course-1/part-2/part02-Part02_14.Counting/src/main/java/Counting.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Counting {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.valueOf(scanner.next());
+        
+        for (int i = 0; i <= number; i++) {
+            System.out.println(i);
+        }
+    }
+}
