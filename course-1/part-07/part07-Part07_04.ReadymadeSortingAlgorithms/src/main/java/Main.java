c45e551447ff82b452443d766c0b6fe8c2354902
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 16:00:52 +0100

    add: Part07-04 - ReadymadeSortingAlgorithms

---
+++ course-1/part-07/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> integers) {
+        Collections.sort(integers);
+    }
+
+    public static void sortStrings(ArrayList<String> strings) {
+        Collections.sort(strings);
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
