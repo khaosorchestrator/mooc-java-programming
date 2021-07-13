54888212ad3bb32b7a10e0480d60b601f91fe26d
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 15:27:49 +0100

    add: Part04-07 - Product

---
+++ course-1/part-04/part04-Part04_07.Product/src/main/java/Product.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lucas
+ */
+public class Product {
+
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public void printProduct() {
+        System.out.println(name + ", price " + price + ", " + quantity + " pcs");
+    }
+}
