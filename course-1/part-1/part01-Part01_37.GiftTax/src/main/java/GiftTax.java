41739fee446cd4607acd2885f71b670b0ea6102a
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 14:46:59 +0100

    add: Part01-37 - GiftTax

---
+++ course-1/part-1/part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class GiftTax {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Value of the gift?");
+        int gift = Integer.valueOf(scan.nextLine());
+        double tax = 0.0;
+
+        if (gift < 5000) {
+            System.out.println("No tax!");
+        } else if (gift >= 5000 && gift < 25000) {
+            tax = 100 + (gift - 5000) * 0.08;
+        } else if (gift >= 25000 && gift < 55000) {
+            tax = 1700 + (gift - 25000) * 0.1;
+        } else if (gift >= 55000 && gift < 200000) {
+            tax = 4700 + (gift - 55000) * 0.12;
+        } else if (gift >= 200000 && gift < 1000000) {
+            tax = 22100 + (gift - 200000) * 0.15;
+        } else if (gift >= 1000000) {
+            tax = 142100 + (gift - 1000000) * 0.17;
+        }
+
+        System.out.println("Tax: " + tax);
+    }
+}
