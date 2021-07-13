bc65057d0fb514df349b3d713de9f37726ee355d
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 15:32:24 +0100

    add: Part04-21 - NumberOfStrings

---
+++ course-1/part-04/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class NumberOfStrings {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+
+        while (true) {
+            String string = scanner.nextLine();
+
+            if (string.equals("end")) {
+                break;
+            }
+            
+            count++;
+        }
+        
+        System.out.println(count);
+    }
+}
