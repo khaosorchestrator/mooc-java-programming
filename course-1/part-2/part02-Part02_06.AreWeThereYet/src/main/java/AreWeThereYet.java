6148d92114dba8c170bcd1596d87c02db4f338db
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 14:50:23 +0100

    add: Part02-06 - AreWeThereYet

---
+++ course-1/part-2/part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class AreWeThereYet {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.next());
+
+            if (number == 4) {
+                break;
+            }
+
+            System.out.println(number);
+        }
+
+    }
+}
