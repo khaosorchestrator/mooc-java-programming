f10870f0d459945117b28747aa813526700576a3
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 16:20:09 +0100

    add: Part10-05 - PrintingUserInput

---
+++ course-2/part-10/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PrintingUserInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> inputs = new ArrayList<>();
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            inputs.add(input);
+        }
+
+        inputs.stream().forEach(System.out::println);
+    }
+}
