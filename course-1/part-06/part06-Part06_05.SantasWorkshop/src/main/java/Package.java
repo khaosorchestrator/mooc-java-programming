535ea916d5293294ca3049e60dae3b07ae258cd8
Lucas Almeida <developer.lucas24@gmail.com>
2021-07-13 15:53:57 +0100

    add: Part06-05 - SantasWorkshop

---
+++ course-1/part-06/part06-Part06_05.SantasWorkshop/src/main/java/Package.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author lucas
+ */
+public class Package {
+
+    private ArrayList<Gift> gifts;
+
+    public Package() {
+        this.gifts = new ArrayList<>();
+    }
+
+    public void addGift(Gift gift) {
+        if (!this.gifts.contains(gift)) {
+            this.gifts.add(gift);
+        }
+    }
+
+    public int totalWeight() {
+        int total = 0;
+
+        for (Gift gift : gifts) {
+            total += gift.getWeight();
+        }
+
+        return total;
+    }
+}
