bec09c19ffcec35febcc8d6f8c766381e13a96d6
Lucas Almeida <developer.lucas24@gmail.com>
2021-08-03 01:46:58 +0100

    refactor: remove repited code

--- java-programming-II/part-12/part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
+++ java-programming-II/part-12/part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
@@ -12,30 +12,23 @@ public class MagicSquareFactory {
         boolean isValidRow;
         boolean isValidColumn;
 
-        square.placeValue(column, row, number++);
         while (number <= SIZE) {
             isValidRow = square.readValue(row - 1) != -1;
             isValidColumn = square.readValue(column + 1) != -1;
-
+            square.placeValue(column, row, number++);
+            
             if (!isValidRow && isValidColumn) {
-                if (square.readValue(size - 1, ++column) == 0) {
-                    row = size - 1;
-                    square.placeValue(column, row, number++);
-                }
+                if (square.readValue(size - 1, ++column) == 0) row = size - 1;
             } else if (isValidRow && !isValidColumn) {
-                if (square.readValue(--row, 0) == 0) {
-                    square.placeValue(0, row, number++);
-                    column = 0;
-                }
+                if (square.readValue(--row, 0) == 0) column = 0;
             } else if (isValidRow && isValidColumn) {
-                if (square.readValue(row - 1, column + 1) != 0) {
-                    square.placeValue(column, ++row, number++);
-                } else {
-                    square.placeValue(++column, --row, number++);
+                if (square.readValue(row - 1, column + 1) != 0) ++row;
+                else {
+                    ++column;
+                    --row;
                 }
-            } else {
-                square.placeValue(column, ++row, number++);
-            }
+            } else ++row;
+            
         }
         return square;
     }
